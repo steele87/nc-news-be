c8538b64d7ced19d65391ff589cf94bb4107fa99
Sam C <samuel.g.caine@gmail.com>
2017-09-27 14:32:17 +0100

    add process.env.NODE_ENV to port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = require('./config').PORT;
+const PORT = require('./config').PORT[process.env.NODE_ENV];
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
