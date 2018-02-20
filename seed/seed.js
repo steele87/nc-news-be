0f106580b6d06b0ba2b719ed58be813985c6664c
James Steele <jamessteele87@outlook.com>
2018-02-20 14:53:12 +0000

    fix linting

--- seed/seed.js
+++ seed/seed.js
@@ -54,7 +54,7 @@ function addNorthcoderUser(done) {
 }
 
 function addUsers(done) {
-  logger.info('adding users')
+  logger.info('adding users');
   async.eachSeries(userData, function (user, cb) {
     var userDoc = new models.Users(user);
     userDoc.save(function (err) {
@@ -65,12 +65,12 @@ function addUsers(done) {
     });
   }, function (error) {
     if (error) return done(error);
-    return done(null)
-  })
+    return done(null);
+  });
 }
 
 function addTopics(done) {
-  logger.info('adding topics')
+  logger.info('adding topics');
   var topicDocs = [];
   async.eachSeries(['Football', 'Cooking', 'Coding'], function (topic, cb) {
     var topicObj = {
@@ -89,8 +89,8 @@ function addTopics(done) {
     });
   }, function (error) {
     if (error) return done(error);
-    return done(null, topicDocs)
-  })
+    return done(null, topicDocs);
+  });
 }
 
 function addArticles(topicDocs, done) {
@@ -130,12 +130,12 @@ function addArticles(topicDocs, done) {
     }, function (error) {
       if (error) return cb(error);
       return cb(null, docIds);
-    })
+    });
 
   }, function (error) {
     if (error) return done(error);
-    return done(null, docIds)
-  })
+    return done(null, docIds);
+  });
 }
 
 function addComments(docIds, done) {
@@ -152,26 +152,26 @@ function addComments(docIds, done) {
       var commentDoc = new models.Comments(comment);
       commentDoc.save(function (err) {
         if (err) {
-          return cb(err)
+          return cb(err);
         }
         return cbTwo();
-      })
+      });
     }, function (error) {
       if (error) return done(error);
       return cb();
-    })
+    });
 
   }, function (err) {
     if (err) return done(err);
-    return done()
+    return done();
   });
 }
 
 function getRandomStamp() {
   return new Date (
     moment().subtract(_.sample(_.range(1,7)), 'days')
-    .subtract(_.sample(_.range(1,24)), 'hours')
-    .subtract(_.sample(_.range(1,60)), 'minutes')
-    .format()
-  ).getTime()
+      .subtract(_.sample(_.range(1,24)), 'hours')
+      .subtract(_.sample(_.range(1,60)), 'minutes')
+      .format()
+  ).getTime();
 }
